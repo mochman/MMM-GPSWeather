878f3a320f924c3761799c2d61b209cf3ef5d3b8
mochman <lukemoch@gmail.com>
2016-11-10 13:04:48 -0500

    Updated gps.php for time

--- server-files/gps.php
+++ server-files/gps.php
@@ -1,9 +1,8 @@
-<?php
+<?php 
 
 // THIS IS ONLY A SAMPLE SCRIPT. PLEASE TWEAK IT TO YOUR NEEDS.
-// IT COMES WITH NO WARRANTY WHATSOEVER. 
-
-$file = "./tmp/gps-position.php"; // you might save in a database instead...
+// IT COMES WITH NO WARRANTY WHATSOEVER. $file = "./tmp/gps-position.php";
+// you might save in a database instead...
 
 if (isset($_GET["lat"]) && preg_match("/^-?\d+\.\d+$/", $_GET["lat"])
     && isset($_GET["lon"]) && preg_match("/^-?\d+\.\d+$/", $_GET["lon"]) ) {
@@ -12,9 +11,11 @@ if (isset($_GET["lat"]) && preg_match("/^-?\d+\.\d+$/", $_GET["lat"])
         header("HTTP/1.0 500 Internal Server Error");
         exit(print_r(error_get_last(), true));
     }
-    fwrite($fh, "{\"lat\":".$_GET["lat"].",\"lon\":".$_GET["lon"]."}");
+    fwrite($fh, "{\"lat\":".$_GET["lat"].",\"lon\":".$_GET["lon"]);
     if (isset($_GET["t"]) && preg_match("/^\d+$/", $_GET["t"])) {
-        fwrite($fh, $_GET["t"]);
+	fwrite($fh, ",\"time\":".$_GET["t"]."}");
+    } else {
+	fwrite($fh, "}");
     }
     fclose($fh);
     // you should obviously do your own checks before this...
